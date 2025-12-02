1f681c340377b328fa357a73847428d60f836821
Mayara | Explorando IA & Criatividade <202510068412@alunos.estacio.br>
2025-12-02 11:22:26 -0300

    Create case_de_negocio_churn.sql

---
+++ queries/queries/queries/queries/queries/case_de_negocio_churn.sql
@@ -0,0 +1,19 @@
+WITH activity AS (
+    SELECT
+        customer_id,
+        MAX(login_date) AS last_login,
+        MAX(payment_date) AS last_payment
+    FROM events
+    GROUP BY 1
+)
+SELECT
+    customer_id,
+    last_login,
+    last_payment,
+    CASE
+        WHEN last_login < CURRENT_DATE - INTERVAL '30 days'
+        OR last_payment < CURRENT_DATE - INTERVAL '30 days'
+        THEN 'Risco Alto'
+        ELSE 'OK'
+    END AS churn_risk
+FROM activity;
